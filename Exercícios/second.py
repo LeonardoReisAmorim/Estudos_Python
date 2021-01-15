27352f7d70d878d73cfe3cc9761aa56e23e1d3eb
Leonardo Reis Amorim <leoreisamorim@gmail.com>
2021-01-15 20:17:19 -0300

    up

--- Exercícios/second.py
+++ Exercícios/second.py
@@ -2,7 +2,7 @@
 O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 Faça um script que leia o nome dos quatro alunos e mostre a ordem sorteada
 """
-from random import choices #importei a biblioteca random com a função que faz uma ordem aleatoria
+from random import shuffle #importei a biblioteca random com a função de embaralhamento
 
 i = 1 #inicializei o contador do loop
 alunos = [] #inicializei o vetor
@@ -12,6 +12,7 @@ while i <= 4:
     alunos.append(input("digite o nome do aluno"))
     i+=1
 
+# o metodo shuffle embaralha a lista
+shuffle(alunos)
 # mostrei os nomes dos alunos
-print(choices(alunos, k=len(alunos)))
-#o primeiro parametro é o array, e o segundo é até qtd de casas(array) que vai ser ordenado, no caso como tem quatro alunos, irá na ordem de apresentação aleatoria
+print(alunos)
